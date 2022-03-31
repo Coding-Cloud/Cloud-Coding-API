f2d8448b7b6aeb797f877c893536d445de14722a
pooetitu <pooetitu@gmail.com>
2022-04-01 00:31:30 +0200

    [CC-44]: Add join and leave room

--- src/infrastructure/web/gateways/project-edition/project-edition.gateway.ts
+++ src/infrastructure/web/gateways/project-edition/project-edition.gateway.ts
@@ -1,19 +1,17 @@
 import {
   ConnectedSocket,
   OnGatewayConnection,
-  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  WsResponse,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { CodeRunnerApi } from '../../../code-runner/code-runner-api.abstract';
 import { Inject } from '@nestjs/common';
 
 @WebSocketGateway()
-export class ProjectEditionGateway
-  implements OnGatewayConnection, OnGatewayDisconnect
-{
+export class ProjectEditionGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
@@ -21,22 +19,21 @@ export class ProjectEditionGateway
     @Inject(CodeRunnerApi) private readonly codeRunnerApi: CodeRunnerApi,
   ) {}
 
-  handleConnection(client: Socket, ...args: any[]): any {
-    console.log(args);
-    client.join('');
+  handleConnection(client: Socket): void {
+    client.rooms.forEach((room) => client.leave(room));
+    client.join(client.handshake.query.projectId);
+    client.on('disconnecting', () => {
+      client.rooms.forEach((room) => {
+        if (this.server.sockets.adapter.rooms.get(room).size === 1) {
+          console.log('aaa');
+          //this.codeRunnerApi.stopCodeRunner(room);
+        }
+      });
+    });
   }
 
   @SubscribeMessage('rooms')
-  findAll(@ConnectedSocket() client: Socket): Set<string> {
-    return client.rooms;
-  }
-
-  handleDisconnect(client: Socket): void {
-    client.rooms.forEach((room) => {
-      if (room.length === 1) {
-        this.codeRunnerApi.stopCodeRunner(room);
-      }
-    });
-    client.disconnect();
+  getRooms(@ConnectedSocket() client: Socket): WsResponse<string[]> {
+    return { data: Array.from(client.rooms), event: 'rooms' };
   }
 }
