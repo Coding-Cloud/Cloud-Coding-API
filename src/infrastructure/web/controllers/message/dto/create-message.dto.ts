85a91fa0852d4b2b3b946a60fea738ef972d7b3d
pooetitu <pooetitu@gmail.com>
2022-05-23 23:31:44 +0200

    [CC-153]: Fix assets being non nullable in messages

--- src/infrastructure/web/controllers/message/dto/create-message.dto.ts
+++ src/infrastructure/web/controllers/message/dto/create-message.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsUUID, MaxLength, MinLength } from 'class-validator';
+import {
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateMessageDTO {
   @IsString()
@@ -9,6 +15,7 @@ export class CreateMessageDTO {
   content: string;
 
   @IsUUID()
+  @IsOptional()
   @ApiProperty()
   assetId: string;
 }
